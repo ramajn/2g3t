8ea60631785536585ca9ca456bfa5c5e89f55fd8
lauraferruzz <108936656+lauraferruzz@users.noreply.github.com>
2023-09-11 11:41:07 -0300

    Add files via upload

---
+++ script/principal.js
@@ -0,0 +1,9 @@
+ escrever = (msg) => alert(msg);
+ soma =(a,b) => a + b;
+ sub =(a,b) => a - b;
+ div =(a,b) => a / b;
+ mult =(a,b) => a * b;
+ raiz = a => Math.sqrt(a);
+ eq2grau = (a,b,c) => {
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+ }
