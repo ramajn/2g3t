0cb1039474ddafd712cfde719792cc3e5aa9e0d8
ramajn <110238183+ramajn@users.noreply.github.com>
2023-09-18 11:07:17 -0300

    Update principal.js

--- script/principal.js
+++ script/principal.js
@@ -1,9 +1,16 @@
- escrever = (msg) => alert(msg);
- soma =(a,b) => a + b;
- sub =(a,b) => a - b;
- div =(a,b) => a / b;
- mult =(a,b) => a * b;
- raiz = a => Math.sqrt(a);
- eq2grau = (a,b,c) => {
+escrever = (msg) => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+div = (a,b) => a / b;
+mult = (a,b) => a * b;
+raiz = a => Math.sqrt(a);
+eq2grau = (a,b,c) => {
+    if(a == 0) return "Não é uma equação do Segundo Grau.";
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
- }
+    if(delta < 0) return "Não Possui raiz real.";
+    if(delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 " + div(soma(-b,raiz(delta)),mult(2,a)) + 
+           "x2 " + div(sub(-b,raiz(delta)),mult(2,a));
+}
+// x2 + 3x -8 = 0
+escrever(eq2grau(1,3,-8));
